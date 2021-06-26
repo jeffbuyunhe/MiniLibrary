8e1f796f52bac5fa161dbe51f5f04b4ed219f743
jeffbuyunhe <jeffbuyunhe@outlook.com>
2021-06-26 19:35:28 -0400

    Small Bug Fixes 01
    Fixed bug with activity_book's layout, modified link in main activity to take user to proper repository

--- app/src/main/java/com/example/minilibrary/MainActivity.java
+++ app/src/main/java/com/example/minilibrary/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Intent intent = new Intent(MainActivity.this, WebsiteActivity.class);
-                        intent.putExtra("url", "https://github.com/jeffbuyunhe/personalprojects");
+                        intent.putExtra("url", "https://github.com/jeffbuyunhe/MiniLibrary");
                         startActivity(intent);
                     }
                 });
